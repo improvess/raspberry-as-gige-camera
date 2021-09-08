d44e776d135fb50967e8e98155c3698d66425e1f
doleron <doleron@gmail.com>
2021-09-08 00:53:02 -0300

    remove client src files

--- code/client/cpp_api/include/rpiasgige/client_api.hpp
+++ code/client/cpp_api/include/rpiasgige/client_api.hpp
@@ -1,6 +1,13 @@
 #ifndef RPIASGIGE_CLIENT_API_HPP
 #define RPIASGIGE_CLIENT_API_HPP
 
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 #include <chrono>
 
 #include <opencv2/opencv.hpp>
@@ -42,7 +49,7 @@ namespace rpiasgige
             int data_size;
             char *data;
 
-            Packet(char *status, bool keep_alive, int data_size, char *data) : status(status), keep_alive(keep_alive), data_size(data_size), data(data) {}
+            Packet(char *status, bool keep_alive, int data_size, char *data) : status(status), keep_alive(keep_alive = false), data_size(data_size), data(data) {}
 
             /**
              * return true if the packet status is equal to the parameter query
@@ -104,15 +111,165 @@ namespace rpiasgige
 
             virtual ~Device() {}
 
-            bool set(int propId, double value, bool keep_alive = false);
-            double get(int propId, bool keep_alive = false);
-            bool open(bool keep_alive = false);
-            bool isOpened(bool keep_alive = false);
-            bool retrieve(cv::Mat &dest, bool keep_alive = false);
-            bool release(bool keep_alive = false);
-            bool ping(bool keep_alive = false);
+            bool ping(bool keep_alive = false)
+            {
+                bool result = "";
+                try
+                {
+                    Packet request(this->request_buffer, keep_alive, 0, this->request_buffer + HEADER_SIZE);
+                    request.set_status("PING");
 
-            void set_read_timeout(int timeout_in_seconds);
+                    Packet response(this->response_buffer, keep_alive, 0, this->response_buffer + HEADER_SIZE);
+                    this->send_request(request, response);
+                    result = response.check_if_status_is("PONG");
+                }
+                catch (TimeoutException &tex)
+                {
+                    this->handle_timeout(tex);
+                }
+
+                return result;
+            }
+
+            double get(int propId, bool keep_alive = false)
+            {
+                double result = -1.0;
+                try
+                {
+                    const int data_size = sizeof(int);
+                    Packet request(this->request_buffer, keep_alive, data_size, this->request_buffer + HEADER_SIZE);
+                    request.set_status("GET0");
+                    memcpy(request.data, &propId, sizeof(int));
+
+                    Packet response(this->response_buffer, keep_alive, 0, this->response_buffer + HEADER_SIZE);
+                    this->send_request(request, response);
+                    this->read_response_data(&result, sizeof(result), response);
+                }
+                catch (TimeoutException &tex)
+                {
+                    this->handle_timeout(tex);
+                }
+                return result;
+            }
+
+            bool set(int propId, double value, bool keep_alive = false)
+            {
+                bool result = false;
+                try
+                {
+                    const int data_size = sizeof(int) + sizeof(double);
+                    Packet request(this->request_buffer, keep_alive, data_size, this->request_buffer + HEADER_SIZE);
+                    request.set_status("SET0");
+                    memcpy(request.data, &propId, sizeof(int));
+                    memcpy(request.data + sizeof(int), &value, sizeof(double));
+
+                    Packet response(this->response_buffer, keep_alive, 0, this->response_buffer + HEADER_SIZE);
+                    this->send_request(request, response);
+                    result = response.check_if_status_is("0200");
+                }
+                catch (TimeoutException &tex)
+                {
+                    this->handle_timeout(tex);
+                }
+
+                return result;
+            }
+
+            bool open(bool keep_alive = false)
+            {
+                bool result = false;
+                try
+                {
+                    Packet request(this->request_buffer, keep_alive, 0, this->request_buffer + HEADER_SIZE);
+                    request.set_status("OPEN");
+
+                    Packet response(this->response_buffer, keep_alive, 0, this->response_buffer + HEADER_SIZE);
+                    this->send_request(request, response);
+                    result = response.check_if_status_is("0200");
+                }
+                catch (TimeoutException &tex)
+                {
+                    this->handle_timeout(tex);
+                }
+                return result;
+            }
+
+            bool isOpened(bool keep_alive = false)
+            {
+                bool result = false;
+                try
+                {
+                    Packet request(this->request_buffer, keep_alive, 0, this->request_buffer + HEADER_SIZE);
+                    request.set_status("ISOP");
+
+                    Packet response(this->response_buffer, keep_alive, 0, this->response_buffer + HEADER_SIZE);
+                    this->send_request(request, response);
+                    result = response.check_if_status_is("0200");
+                }
+                catch (TimeoutException &tex)
+                {
+                    this->handle_timeout(tex);
+                }
+                return result;
+            }
+
+            bool retrieve(cv::Mat &dest, bool keep_alive = false)
+            {
+                bool result = false;
+                try
+                {
+                    Packet request(this->request_buffer, keep_alive, 0, this->request_buffer + HEADER_SIZE);
+                    request.set_status("GRAB");
+
+                    Packet response(this->response_buffer, keep_alive, 0, this->response_buffer + HEADER_SIZE);
+                    this->send_request(request, response);
+                    result = response.check_if_status_is("0200");
+                    if (result)
+                    {
+                        const char *data = response.data;
+                        int size_int = sizeof(int);
+                        const int *rows = (int *)data;
+                        const int *cols = (int *)(data + size_int);
+                        const int *type = (int *)(data + 2 * size_int);
+                        cv::Mat temp = cv::Mat::zeros(*rows, *cols, *type);
+                        temp.data = (unsigned char *)response.data + 3 * size_int;
+                        dest = temp;
+                    }
+                }
+                catch (TimeoutException &tex)
+                {
+                    this->handle_timeout(tex);
+                }
+                return result;
+            }
+            
+            bool release(bool keep_alive = false)
+            {
+                bool result = false;
+                try
+                {
+                    Packet request(this->request_buffer, keep_alive, 0, this->request_buffer + HEADER_SIZE);
+                    request.set_status("CLOS");
+
+                    Packet response(this->response_buffer, keep_alive, 0, this->response_buffer + HEADER_SIZE);
+                    this->send_request(request, response);
+                    result = response.check_if_status_is("0200");
+                }
+                catch (TimeoutException &tex)
+                {
+                    this->handle_timeout(tex);
+                }
+                return result;
+            }
+
+            void set_read_timeout(int timeout_in_seconds)
+            {
+                if (this->read_timeout_in_seconds >= 0)
+                {
+                    this->read_timeout_in_seconds = timeout_in_seconds;
+                }
+                this->update_socket_timeout();
+            }
 
         private:
             cv::String address;
@@ -121,13 +278,108 @@ namespace rpiasgige
             const int MAX_TIMEOUT_COUNT = 5;
             int read_timeout_in_seconds = 1;
 
-            void handle_timeout(TimeoutException &tex);
+            void handle_timeout(TimeoutException &tex)
+            {
+                std::cout << tex.what() << "\n";
 
-            void send_request(const Packet &request, Packet &response);
+                if (timeout_count < MAX_TIMEOUT_COUNT)
+                {
+                    timeout_count++;
+                }
+                else
+                {
+                    timeout_count = 0;
+                    this->disconnect();
+                }
+            }
 
-            bool open_tcp_conversation();
+            void send_request(const Packet &request, Packet &response)
+            {
+                memset(response_buffer, 0, response_buffer_size);
 
-            void update_socket_timeout();
+                bool result = false;
+                if (!this->is_connected())
+                {
+                    this->open_tcp_conversation();
+                }
+
+                if (this->is_connected())
+                {
+                    if (request.keep_alive)
+                    {
+                        request_buffer[KEEP_ALIVE_ADDRESS] = '1';
+                    }
+                    else
+                    {
+                        request_buffer[KEEP_ALIVE_ADDRESS] = '0';
+                    }
+
+                    this->set_request_data_size(request.data_size);
+
+                    if (this->send_request_buffer(request.data_size + HEADER_SIZE))
+                    {
+
+                        this->read_response(response);
+
+                        if (!request.keep_alive)
+                        {
+                            this->disconnect();
+                        }
+                    }
+                    else
+                    {
+                        if (!request.keep_alive)
+                        {
+                            this->disconnect();
+                        }
+                        throw TimeoutException{"Failed to send data to server."};
+                    }
+                }
+                else
+                {
+                    throw RemoteException{"Not connected"};
+                }
+            }
+
+            bool open_tcp_conversation()
+            {
+
+                if ((this->server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+                {
+                    this->server_socket = -1;
+                }
+
+                struct sockaddr_in serv_addr;
+                serv_addr.sin_family = AF_INET;
+                serv_addr.sin_port = htons(this->port);
+
+                if (inet_pton(AF_INET, this->address.c_str(), &serv_addr.sin_addr) <= 0)
+                {
+                    close(this->server_socket);
+                    this->server_socket = -1;
+                }
+
+                if (connect(this->server_socket, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+                {
+                    close(this->server_socket);
+                    this->server_socket = -1;
+                }
+                this->update_socket_timeout();
+                return this->is_connected();
+            }
+
+            void update_socket_timeout()
+            {
+                if (this->server_socket >= 0 && this->read_timeout_in_seconds >= 0)
+                {
+                    struct timeval tv;
+                    tv.tv_sec = this->read_timeout_in_seconds;
+                    // minimum acceptable timeout is 10 ms
+                    tv.tv_usec = 10000;
+                    setsockopt(this->server_socket, SOL_SOCKET, SO_RCVTIMEO, (const char *)&tv, sizeof tv);
+
+                }
+            }
 
             int server_socket = -1;
 
@@ -142,22 +394,112 @@ namespace rpiasgige
                 return server_socket >= 0;
             }
 
-            void disconnect();
+            void disconnect()
+            {
+                if (this->server_socket >= 0)
+                {
+                    close(this->server_socket);
+                    this->server_socket = -1;
+                }
+            }
 
             /* *
              * Load the response into the response parameter
              * */
-            bool read_response(Packet &response);
+            bool read_response(Packet &response)
+            {
+
+                int total_read = 0;
+                bool keep = true;
+                int no_response_count = 0;
+                const int max_no_response_count = 3;
+                int expected_response_size = HEADER_SIZE;
+
+                int data_size = 0;
+                while (keep)
+                {
+                    int packt_size = std::min(1024, this->response_buffer_size - total_read);
+                    int valread = recv(this->server_socket, response_buffer + total_read, packt_size, 0);
+                    if (valread > 0)
+                    {
+                        total_read += valread;
+                        no_response_count = 0;
+
+                        if (data_size == 0 && total_read >= HEADER_SIZE)
+                        {
+                            int expected_data_size;
+                            memcpy(&expected_data_size, response_buffer + DATA_SIZE_ADDRESS, sizeof(expected_data_size));
+
+                            if (expected_data_size > 0 && (HEADER_SIZE + expected_data_size) <= response_buffer_size)
+                            {
+                                data_size = expected_data_size;
+                                expected_response_size = HEADER_SIZE + data_size;
+                                response.data_size = data_size;
+                            }
+                        }
+
+                        keep = total_read < expected_response_size;
+                    }
+                    else
+                    {
+                        no_response_count++;
+                        keep = no_response_count < max_no_response_count;
+                    }
+                }
+
+                int result = std::min(total_read, HEADER_SIZE + data_size);
+                if (result < expected_response_size)
+                {
+                    result = 0;
+                    response.data_size = 0;
+                }
+                return result;
+            }
 
             /**
              * loads into buffer a chunk of data from the response packet
              **/
-            void read_response_data(void *buffer, const int size, const Packet &response);
+            void read_response_data(void *buffer, const int size, const Packet &response)
+            {
+                if (size > 0 && size <= response.data_size)
+                {
+                    memcpy(buffer, response.data, size);
+                }
+            }
 
             /* *
              * helper method to send request
              * */
-            bool send_request_buffer(const int bytes_to_send);
+            bool send_request_buffer(const int bytes_to_send)
+            {
+                bool result = false;
+                if (bytes_to_send <= this->request_buffer_size)
+                {
+                    int sent = 0;
+                    bool keep = true;
+                    int fail_count = 0;
+                    while (keep && sent < bytes_to_send)
+                    {
+
+                        int current_packet_size = std::min(bytes_to_send - sent, 1024);
+
+                        int val_sent = send(this->server_socket, this->request_buffer + sent, current_packet_size, MSG_NOSIGNAL);
+                        if (val_sent > 0)
+                        {
+                            fail_count = 0;
+                            sent += val_sent;
+                        }
+                        else
+                        {
+                            fail_count++;
+                            keep = fail_count < 3;
+                        }
+                    }
+                    result = sent == bytes_to_send;
+                }
+
+                return result;
+            }
 
             void set_request_data_size(int size)
             {
