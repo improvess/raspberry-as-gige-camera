8260b6d5373a72f2127b80ad6825d387796c3a78
doleron <doleron@gmail.com>
2021-10-08 19:23:58 -0300

    flush dumb logger

--- code/server/include/rpiasgige/dumb_logger.hpp
+++ code/server/include/rpiasgige/dumb_logger.hpp
@@ -18,6 +18,7 @@ public:
         struct tm tm = *localtime(&t);
         printf("%d-%02d-%02d %02d:%02d:%02d - ", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
         printf("%s - %s - %s\n", level.c_str(), this->identifier.c_str(), msg.c_str());
+        fflush(stdout);
     }
 
     void error_msg(const std::string &msg) const
