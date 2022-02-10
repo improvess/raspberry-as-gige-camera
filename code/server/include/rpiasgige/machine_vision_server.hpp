d849788697703b368d2f089f8befd733654b5cf9
doleron <doleron@gmail.com>
2022-02-10 10:44:08 -0300

    undo mutex

--- code/server/include/rpiasgige/machine_vision_server.hpp
+++ code/server/include/rpiasgige/machine_vision_server.hpp
@@ -1,9 +1,6 @@
 #ifndef RPIASGIGE_TCP_SERVER_INTERFACE_HPP
 #define RPIASGIGE_TCP_SERVER_INTERFACE_HPP
 
-#include <mutex> 
-#include <chrono>
-
 #include "usb_interface.hpp"
 #include "websocket_server.hpp"
 
@@ -20,24 +17,6 @@ namespace rpiasgige
 
             static const int IMAGE_META_DATA_SIZE = 3 * sizeof(int);
 
-            bool set_camera_timeout_in_milliseconds(const int val) {
-                bool result = false;
-                if (val > 0) {
-                    this->usb_camera_mutex_timeout = std::chrono::milliseconds(val);
-                    result = true;
-                }
-                return result;
-            }
-
-            bool set_camera_open_timeout_in_milliseconds(const int val) {
-                bool result = false;
-                if (val > 0) {
-                    this->usb_camera_open_mutex_timeout = std::chrono::milliseconds(val);
-                    result = true;
-                }
-                return result;
-            }
-
         protected:
 
             virtual void prepare_response(const char * request_buffer, const int request_size, char * response_buffer, int &response_size)
@@ -47,34 +26,31 @@ namespace rpiasgige
 
                 response_buffer[KEEP_ALIVE_ADDRESS] = request_buffer[KEEP_ALIVE_ADDRESS];
 
-                bool camera_timeout = false;
                 if (strncmp("GRAB", request_buffer, STATUS_SIZE) == 0) {
 
-                    if(usb_camera_mutex.try_lock_for(this->usb_camera_mutex_timeout)) {
-                        this->usb_camera.grab();
-                        const cv::Mat &mat = this->usb_camera.get_captured_image();
-                        int image_size = 0;
+                    this->usb_camera.grab();
 
-                        if (!mat.empty()) {
-                            image_size = mat.total() * mat.elemSize();
-                            const char *mat_data = (const char *)mat.data;
-                            int size_int = sizeof(int);
-                            const int metada_data_size = 3*size_int;
-                            this->set_buffer_value(response_buffer, HEADER_SIZE, size_int, &mat.rows);
-                            this->set_buffer_value(response_buffer, HEADER_SIZE + size_int, size_int, &mat.cols);
-                            int type = mat.type();
-                            this->set_buffer_value(response_buffer, HEADER_SIZE + 2*size_int, size_int, &type);
-                            this->set_buffer_value(response_buffer, HEADER_SIZE + metada_data_size, image_size, mat_data);
-                            this->set_status(response_buffer, "0200");
+                    const auto & mat = this->usb_camera.get_captured_image();
 
-                            response_size = HEADER_SIZE + image_size + metada_data_size;
+                    int image_size = 0;
 
-                            set_response_data_size(response_buffer, image_size + metada_data_size);
-                        } else {
-                            this->set_status(response_buffer, "NOPE");
-                        }
+                    if (!mat.empty()) {
+                        image_size = mat.total() * mat.elemSize();
+                        const char *mat_data = (const char *)mat.data;
+                        int size_int = sizeof(int);
+                        const int metada_data_size = 3*size_int;
+                        this->set_buffer_value(response_buffer, HEADER_SIZE, size_int, &mat.rows);
+                        this->set_buffer_value(response_buffer, HEADER_SIZE + size_int, size_int, &mat.cols);
+                        int type = mat.type();
+                        this->set_buffer_value(response_buffer, HEADER_SIZE + 2*size_int, size_int, &type);
+                        this->set_buffer_value(response_buffer, HEADER_SIZE + metada_data_size, image_size, mat_data);
+                        this->set_status(response_buffer, "0200");
+
+                        response_size = HEADER_SIZE + image_size + metada_data_size;
+
+                        set_response_data_size(response_buffer, image_size + metada_data_size);
                     } else {
-                        camera_timeout = true;
+                        this->set_status(response_buffer, "NOPE");
                     }
         
                 }
@@ -88,18 +64,12 @@ namespace rpiasgige
                         memcpy(&propId, request_buffer + HEADER_SIZE, sizeof(int));
                         memcpy(&value, request_buffer + HEADER_SIZE + sizeof(int), sizeof(double));
 
-                        bool result = false;
-                        if(usb_camera_mutex.try_lock_for(this->usb_camera_mutex_timeout)) {
-                            result = this->usb_camera.set(propId, value);
+                        bool result = this->usb_camera.set(propId, value);
+
+                        if (result) {
+                            this->set_status(response_buffer, "0200");
                         } else {
-                            camera_timeout = true;
-                        }
-                        if (!camera_timeout) {
-                            if (result) {
-                                this->set_status(response_buffer, "0200");
-                            } else {
-                                this->set_status(response_buffer, "NOPE");
-                            }
+                            this->set_status(response_buffer, "NOPE");
                         }
 
                     } else {
@@ -107,36 +77,21 @@ namespace rpiasgige
                     }
 
                 } else if (strncmp("OPEN", request_buffer, STATUS_SIZE) == 0) {
-                    bool result = false;
-                    if(usb_camera_mutex.try_lock_for(this->usb_camera_open_mutex_timeout)) {
-                        result = this->usb_camera.open_camera();
-                    } else {
-                        camera_timeout = true;
-                    }
-                    if (!camera_timeout) {
+                    bool result = this->usb_camera.open_camera();
 
-                        if (result) {
-                            this->set_status(response_buffer, "0200");
-                        } else {
-                            this->set_status(response_buffer, "NOPE");
-                        }
+                    if (result) {
+                        this->set_status(response_buffer, "0200");
+                    } else {
+                        this->set_status(response_buffer, "NOPE");
                     }
 
                 } else if (strncmp("CLOS", request_buffer, STATUS_SIZE) == 0) {
-                    bool result = false;
-                    if(usb_camera_mutex.try_lock_for(this->usb_camera_mutex_timeout)) {
-                        result = this->usb_camera.release();
+                    bool result = this->usb_camera.release();
+
+                    if (result) {
+                        this->set_status(response_buffer, "0200");
                     } else {
-                        camera_timeout = true;
-                    }
-
-                    if (!camera_timeout) {
-
-                        if (result) {
-                            this->set_status(response_buffer, "0200");
-                        } else {
-                            this->set_status(response_buffer, "NOPE");
-                        }
+                        this->set_status(response_buffer, "NOPE");
                     }
 
                 } else if (strncmp("GET0", request_buffer, STATUS_SIZE) == 0) {
@@ -144,36 +99,23 @@ namespace rpiasgige
                     int propId;
 
                     memcpy(&propId, request_buffer + HEADER_SIZE, sizeof(int));
-                    double value = -1;
-                    if(usb_camera_mutex.try_lock_for(this->usb_camera_mutex_timeout)) {
-                        value = this->usb_camera.get(propId);
-                    } else {
-                        camera_timeout = true;
-                    }
-                    if (!camera_timeout) {
 
-                        this->set_buffer_value(response_buffer, HEADER_SIZE, SIZE_OF_DOUBLE, &value);
+                    double value = this->usb_camera.get(propId);
 
-                        this->set_response_data_size(response_buffer, SIZE_OF_DOUBLE);
+                    this->set_buffer_value(response_buffer, HEADER_SIZE, SIZE_OF_DOUBLE, &value);
 
-                        response_size = HEADER_SIZE + sizeof(double);
-                        this->set_status(response_buffer, "0200");
-                    }
+                    this->set_response_data_size(response_buffer, SIZE_OF_DOUBLE);
+
+                    response_size = HEADER_SIZE + sizeof(double);
+                    this->set_status(response_buffer, "0200");
 
                 } else if (strncmp("ISOP", request_buffer, STATUS_SIZE) == 0) {
-                    bool result = false;
-                    if(usb_camera_mutex.try_lock_for(this->usb_camera_mutex_timeout)) {
-                        result = this->usb_camera.isOpened();
-                    } else {
-                        camera_timeout = true;
-                    }
-                    if (!camera_timeout) {
+                    bool result = this->usb_camera.isOpened();
 
-                        if (result) {
-                            this->set_status(response_buffer, "0200");
-                        } else {
-                            this->set_status(response_buffer, "NOPE");
-                        }
+                    if (result) {
+                        this->set_status(response_buffer, "0200");
+                    } else {
+                        this->set_status(response_buffer, "NOPE");
                     }
 
                 } else if (strncmp("PING", request_buffer, STATUS_SIZE) == 0) {
@@ -181,16 +123,10 @@ namespace rpiasgige
                 } else {
                     this->set_status(response_buffer, "0404");
                 }
-                if (camera_timeout) {
-                    this->set_status(response_buffer, "TIME");
-                }
             }
 
         private:
             USB_Interface &usb_camera;
-            std::timed_mutex usb_camera_mutex;
-            std::chrono::milliseconds usb_camera_mutex_timeout = std::chrono::milliseconds(200);
-            std::chrono::milliseconds usb_camera_open_mutex_timeout = std::chrono::milliseconds(1000);
             static const int SIZE_OF_DOUBLE = sizeof(double);
             static const int SET_DATA_SIZE = sizeof(int) + SIZE_OF_DOUBLE;
         };
